df7b4800c01587d8e5a655247e73b4454d09ff47
Mateusz Zagórski <01161597@pw.edu.pl>
2022-10-18 21:10:46 +0200

    Initial and final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import GameAdmin from './GameAdmin.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <GameAdmin numberOfPlayers = {2} initialName1="name1" initialName2="name2" initialCounter1="0" initialCounter2="0"/>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
