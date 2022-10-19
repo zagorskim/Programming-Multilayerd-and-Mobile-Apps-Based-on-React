9a53f07280c3f5611193db595e34a8fa7c0d0efb
Mateusz Zagórski <01161597@pw.edu.pl>
2022-10-20 01:28:43 +0200

    Added item deletion

--- lab3/src/components/CarsListComponent.tsx
+++ lab3/src/components/CarsListComponent.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, Key} from 'react';
 import {Car, CARS} from "../data/Car";
 import { CarsListItemComponent } from './CarsListItemComponent';
 import SearchBar from './SearchBar';
@@ -6,6 +6,13 @@ import SearchBar from './SearchBar';
 export const CarsListComponent =() => {
     const [carsList, setCarsList] = useState<Car[]>(CARS);
     const [query, setQuery] = useState('');
+
+    function deleteCar(id: Key): void{
+        setCarsList(carsList.slice(0, carsList.findIndex((car) => car.id == id))
+            .concat(carsList.slice(carsList.findIndex((car) => car.id == id) + 1)));
+        // setCarsList(carsList.filter(car => car.id != id));
+    }
+
     return ( 
         <>
             <SearchBar setQuery = {setQuery} />
@@ -13,9 +20,9 @@ export const CarsListComponent =() => {
                 {
                 carsList
                 .filter(carObject => carObject.name.toLowerCase().includes(query.toLowerCase()))
-                .map(carObject => <CarsListItemComponent car={carObject}/>)
+                .map(carObject => <CarsListItemComponent car={carObject} deleteCar={deleteCar}/>)
             }
             </div>
         </>
-    )
+    ) 
 }
--- lab3/src/components/CarsListItemComponent.tsx
+++ lab3/src/components/CarsListItemComponent.tsx
@@ -1,16 +1,17 @@
 import { Car } from "../data/Car";
 import "./Styles/CarsListItemComponent.css";
+import { useState, Key } from 'react';
 
 export interface CarsListItemProps {
-    car: Car
+    car: Car;
+    deleteCar: (id: Key) => void;
 }
 
 export const CarsListItemComponent = (props:CarsListItemProps) =>{
     return (
             <div className="itemContainer">
-                    <img src={props.car.image} className="carImage">
+                    <img src={props.car.image} className="carImage"/>
 
-                    </img>
                 <h1 className="carModel"> 
                     {props.car.name}
                 </h1>
@@ -21,10 +22,10 @@ export const CarsListItemComponent = (props:CarsListItemProps) =>{
                 </div>
                 <div className="priceContainer">
                     <label>Price per day:</label>
-                    <h1 className="carPrice">PLNasdfasdfasdfasdfasdf</h1>
+                    <h1 className="carPrice">PLN {props.car.pricePerDay}</h1>
                     <div className="buttonContainer">
                         <button>Edit</button>
-                        <button>Delete</button>
+                        <button onClick={(e) => {props.deleteCar(props.car.id)}}>Delete</button>
                     </div>
                 </div>
             </div>
