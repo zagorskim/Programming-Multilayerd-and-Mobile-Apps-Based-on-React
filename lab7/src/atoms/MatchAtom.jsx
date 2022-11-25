a8481594c5333033cc0332f3852472bc59f42059
Mateusz Zagórski <01161597@pw.edu.pl>
2022-11-25 01:58:13 +0100

    Fixed clearing country name bug with get query timeout

--- lab7/src/atoms/MatchAtom.jsx
+++ lab7/src/atoms/MatchAtom.jsx
@@ -24,13 +24,14 @@ export const scoreState = atom({
 export const teamNameSelector = selector({
   key: "teamNameSelector",
   get: ({ get }) => {
+    // timeout prevents rejected queries to disrupt logic after setting successful value queried earlier
     return [
       Axios.get(
         "https://restcountries.com/v3.1/alpha/" + get(countryCodeState)[0]
-      ),
+      , {timeout: 20}),
       Axios.get(
         "https://restcountries.com/v3.1/alpha/" + get(countryCodeState)[1]
-      ),
+      , {timeout: 20}),
     ];
   },
 });
